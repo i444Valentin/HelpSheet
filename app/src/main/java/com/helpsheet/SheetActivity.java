5feda758b439d71cded58c287c2374d8d08fff6b
Walentin <valession@gmail.com>
2021-03-24 12:21:59 +0300

    Added sheet activity

---
+++ app/src/main/java/com/helpsheet/SheetActivity.java
@@ -0,0 +1,15 @@
+package com.helpsheet;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SheetActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
